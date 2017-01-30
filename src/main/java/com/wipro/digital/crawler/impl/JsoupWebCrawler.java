b491c288b27176fea74493a1dc09d515f149adcd
Nitin Prabhu <prabhunitin61@gmail.com>
2017-01-30 23:04:07 +0000

    Added Crawler class implementation for first test case

--- src/main/java/com/wipro/digital/crawler/impl/JsoupWebCrawler.java
+++ src/main/java/com/wipro/digital/crawler/impl/JsoupWebCrawler.java
@@ -1,6 +1,13 @@
 package com.wipro.digital.crawler.impl;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import com.wipro.digital.crawler.WebCrawler;
 
@@ -13,8 +20,34 @@ public class JsoupWebCrawler implements WebCrawler
 
 	public List<String> processPage(String url)
 	{
-		// TODO Auto-generated method stub
-		return null;
+
+		List<String> urls = new ArrayList<String>();
+
+		Document document;
+		try
+		{
+			document = Jsoup.connect(url).get();
+			// Get all links on the web page
+			Elements elements = document.select("a[href]");
+
+			for (Element link : elements)
+			{
+				String href = link.attr("href");
+
+				if (href.contains("wiprodigital.com"))
+				{
+					urls.add(href);
+				}
+			}
+		}
+		catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return urls;
+
 	}
 
 }
--- src/test/java/com/wipro/digital/crawler/test/WebCrawlerTest.java
+++ src/test/java/com/wipro/digital/crawler/test/WebCrawlerTest.java
@@ -1,12 +1,21 @@
 package com.wipro.digital.crawler.test;
 
+import java.util.List;
+
+import org.junit.Assert;
 import org.junit.Test;
+
+import com.wipro.digital.crawler.WebCrawler;
+import com.wipro.digital.crawler.impl.JsoupWebCrawler;
 
 public class WebCrawlerTest
 {
 	@Test
 	public void test_process_page_when_url_is_passed_then_return_list_of_all_urls()
 	{
+		WebCrawler webCrawler = new JsoupWebCrawler();
 
+		List<String> urls = webCrawler.processPage("http://wiprodigital.com");
+		Assert.assertNotNull(urls);
 	}
 }

6026cd2ec35b68652bf68f6aaee7b3aa26d3979f
Nitin Prabhu <prabhunitin61@gmail.com>
2017-01-30 23:10:42 +0000

    Added exception handling and logging to code

--- src/main/java/com/wipro/digital/crawler/impl/JsoupWebCrawler.java
+++ src/main/java/com/wipro/digital/crawler/impl/JsoupWebCrawler.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -17,35 +18,50 @@ import com.wipro.digital.crawler.WebCrawler;
  */
 public class JsoupWebCrawler implements WebCrawler
 {
+	private static final String HREF = "href";
+	private static final String A_HREF = "a[href]";
+	private static final String WIPRODIGITAL_COM = "wiprodigital.com";
+	private static final Logger logger = Logger.getLogger(JsoupWebCrawler.class);
 
+	/**
+	 * This method processes finds all the URL on the page for the given URL.It
+	 * only provides a list of URL's for the given domain and not for external
+	 * domains in the page. It stores the output to a file.
+	 */
 	public List<String> processPage(String url)
 	{
+		logger.info("URL passed to the web crawler is " + url);
 
 		List<String> urls = new ArrayList<String>();
 
-		Document document;
 		try
 		{
-			document = Jsoup.connect(url).get();
+			Document document = Jsoup.connect(url).get();
+
 			// Get all links on the web page
-			Elements elements = document.select("a[href]");
+			Elements elements = document.select(A_HREF);
 
 			for (Element link : elements)
 			{
-				String href = link.attr("href");
+				String href = link.attr(HREF);
 
-				if (href.contains("wiprodigital.com") && !urls.contains(href))
+				if (href.contains(WIPRODIGITAL_COM) && !urls.contains(href))
 				{
 					urls.add(href);
 				}
 			}
-		}
-		catch (IOException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 
+			logger.info("List of URL returned by web crawler " + urls);
+
+		}
+		catch (IOException ioException)
+		{
+			logger.error("IOExcepion occured while processing web page url " + url + "Exception is " + ioException);
+		}
+		catch (Exception exception)
+		{
+			logger.error("Some excepion occured while processing web page url " + url + "Exception is " + exception);
+		}
 		return urls;
 
 	}

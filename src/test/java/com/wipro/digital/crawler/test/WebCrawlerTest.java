cc504306f18d4972ebd4980e3016dd37bdbc91b9
Nitin Prabhu <prabhunitin61@gmail.com>
2017-01-30 22:54:22 +0000

    Committing Initial project structure with empty test class

---
+++ src/test/java/com/wipro/digital/crawler/test/WebCrawlerTest.java
@@ -0,0 +1,12 @@
+package com.wipro.digital.crawler.test;
+
+import org.junit.Test;
+
+public class WebCrawlerTest
+{
+	@Test
+	public void test_process_page_when_url_is_passed_then_return_list_of_all_urls()
+	{
+
+	}
+}
